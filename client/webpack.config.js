ff0d970e37b892a01d2a5b946cd4de9982d9eae1
Ashlyn Smith <SMITHASHLYN210@YAHOO.COM>
2023-06-08 11:02:00 -0500

    fixed code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
@@ -18,13 +17,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+       
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'jate List'
+      }),
       
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
+      
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        start_url: './',
+        publicPath: './',
+        crossorigin: 'use-credentials',
+        fingerprints: false,
+        inject: true, 
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons') 
+          }
+        ]
+      }),
     ],
 
     module: {
-      rules: [
-        
-      ],
-    },
-  };
+    
+     rules: [
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+         test: /\.m?js$/,
+         exclude: /node_modules/,
+         use: {
+           loader: 'babel-loader',
+           options: {
+             presets: ['@babel/preset-env'],
+             plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+           },
+         },
+       },
+     ],
+   },
+ };
 };
